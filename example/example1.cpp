dfe93b2f9486a66258680d27fdb67e76db8e9669
PHZ76 <2235710879@qq.com>
2018-05-03 18:24:57 -0700

    update

--- example/example1.cpp
+++ example/example1.cpp
@@ -1,5 +1,6 @@
 // PHZ
-// 2018-2-10
+// 2018-2-10 创建demo
+// 2018-5-1  补充注释
 
 #include "RtspServer.h"
 #include "H264Source.h"
@@ -18,29 +19,30 @@
 using namespace std;
 using namespace xop;
 
+// 负责音视频数据转发的线程函数
 void snedFrame(RtspServer* rtspServer, MediaSessionId sessionId, int& clients)
 {
+    static uint32_t audio_pts = 1024;
+        
 	while(1)
 	{
-		if(clients > 0) // MediaSession有客户端在线, 发送音视频数据
+		if(clients > 0) // 媒体会话有客户端在线, 发送音视频数据
 		{
 			{
 				/* 
+                    ... 
 					//获取一帧 H264, 打包
 					xop::AVFrame videoFrame = {0};
 					videoFrame.size = 100000;  // 视频帧大小 
-					videoFrame.timestamp = H264Source::getTimeStamp(); // 时间戳
+					videoFrame.timestamp = H264Source::getTimeStamp(); // 时间戳, 建议使用编码器提供的时间戳
 					videoFrame.buffer.reset(new char[videoFrame.size+1000]);
-					memcpy(videoFrame.buffer.get(), 视频帧数据, videoFrame.size);
+					memcpy(videoFrame.buffer.get(), 视频帧数据, videoFrame.size);					
 					
-					// 推送到服务器进行转发
-					rtspServer->pushFrame(sessionId, channel_0, videoFrame); // 接口 线程安全
+					rtspServer->pushFrame(sessionId, channel_0, videoFrame); // 推送到服务器进行转发
 				*/
 			}
-			
-			static uint32_t audio_pts = 1024;
-			{
-				
+					
+			{				
 				/*
 					//获取一帧 AAC, 打包
 					xop::AVFrame audioFrame = {0};
@@ -56,7 +58,7 @@ void snedFrame(RtspServer* rtspServer, MediaSessionId sessionId, int& clients)
 			}		
 		}
 
-		Timer::sleep(1000);
+		xop::Timer::sleep(1000); 
 	}
 }
 
@@ -64,39 +66,38 @@ int main(int agrc, char **argv)
 {	
 	XOP_Init();
 	
-	// 记录当前客户端数量
-	int clients = 0;
-	std::string ip = NetInterface::getLocalIPAddress();
 	
-	std::shared_ptr<EventLoop> eventLoop(new EventLoop());
-	RtspServer server(eventLoop.get(), ip, 554); 
+	int clients = 0; // 记录当前客户端数量
+	std::string ip = NetInterface::getLocalIPAddress(); //获取网卡ip地址
+	
+	std::shared_ptr<EventLoop> eventLoop(new EventLoop());  
+	RtspServer server(eventLoop.get(), ip, 554);  //创建一个RTSP服务器
 
-	MediaSession *session = MediaSession::createNew("live"); // rtsp://ip/live
-	// 同时传输音视频, track0:h264, track1:aac
+	MediaSession *session = MediaSession::createNew("live"); // 创建一个媒体会话, url: rtsp://ip/live
+    
+	// 会话同时传输音视频, track0:h264, track1:aac
 	session->addMediaSource(xop::channel_0, H264Source::createNew()); 
 	session->addMediaSource(xop::channel_1, AACSource::createNew(44100,2));
-
-	//  开启组播(ip,端口随机生成), 默认使用 RTP_OVER_UDP, RTP_OVER_RTSP
-	// session->startMulticast();
+	
+	// session->startMulticast(); // 开启组播(ip,端口随机生成), 默认使用 RTP_OVER_UDP, RTP_OVER_RTSP
 	
 	// 设置通知回调函数。 在当前会话, 客户端连接或断开会发起通知
 	session->setNotifyCallback([&clients](MediaSessionId sessionId, uint32_t numClients) 
 	{
-		clients = numClients; //获取当前MediaSession客户端数量
+		clients = numClients; //获取当前媒体会话客户端数量
 		cout << "MediaSession" << "(" << sessionId << ") "
 			 << "clients: " << clients << endl;
 	});
 	
 	cout << "rtsp://" << ip << "/" << session->getRtspUrlSuffix() << endl;
-	
-	// 添加session到RtspServer后, session会失效
-	MediaSessionId sessionId = server.addMeidaSession(session); // 接口线程安全
+		
+	MediaSessionId sessionId = server.addMeidaSession(session); // 添加session到RtspServer后, session会失效
+    //server.removeMeidaSession(sessionId); //取消会话, 接口线程安全
+       	 
+	thread t(snedFrame, &server, sessionId, ref(clients)); // 负责音视频数据转发的线程
+	t.detach(); // 后台运行
 
-	// 音视频数据转发线程
-	thread t(snedFrame, &server, sessionId, ref(clients));
-	t.detach();
-
-	// 运行 RtspServer 
+	// 主线程运行 RtspServer 
 	eventLoop->loop();
 	
 	getchar();
