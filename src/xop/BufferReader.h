a83dbdf49d54456f6b561e8f5db8d66517e68df2
PHZ76 <2235710879@qq.com>
2018-05-03 19:28:02 -0700

    update

--- src/xop/BufferReader.h
+++ src/xop/BufferReader.h
@@ -8,103 +8,93 @@
 
 namespace xop
 {
-	class BufferReader
-	{
-	public:	
-		static const uint32_t kInitialSize = 2048;
-		BufferReader(uint32_t initialSize = kInitialSize);
-		~BufferReader();
-	
-		uint32_t readableBytes() const
-		{ 
-			return _writerIndex - _readerIndex; 
-		}
+    class BufferReader
+    {
+    public:	
+        static const uint32_t kInitialSize = 2048;
+        BufferReader(uint32_t initialSize = kInitialSize);
+        ~BufferReader();
 
-		uint32_t writableBytes() const
-		{ 
-			return _buffer.size() - _writerIndex; 
-		}
-	
-		char* peek() 
-		{ 
-			return begin() + _readerIndex; 
-		}
-	
-		const char* peek() const
-		{ return begin() + _readerIndex; }
-	
-		const char* findFirstCrlf() const
-		{    
-			const char* crlf = std::search(peek(), beginWrite(), kCRLF, kCRLF+2);
-			return crlf == beginWrite() ? nullptr : crlf;
-		}
-	
-		const char* findLastCrlf() const
-		{    
-			const char* crlf = std::find_end(peek(), beginWrite(), kCRLF, kCRLF+2);
-			return crlf == beginWrite() ? nullptr : crlf;
-		}
-	
-		void retrieveAll() 
-		{ 
-			_writerIndex=0; 
-			_readerIndex=0; 
-		}
-	
-		void retrieve(size_t len)
-		{
-			if (len <= readableBytes())
-			{
-				_readerIndex += len;
-				if(_readerIndex == _writerIndex)
-				{
-					_readerIndex = 0;
-					_writerIndex = 0;
-				}
-			}
-			else
-			{
-				retrieveAll();
-			}
-		}
+        uint32_t readableBytes() const
+        { return _writerIndex - _readerIndex; }
 
-		void retrieveUntil(const char* end)
-		{
-			retrieve(end - peek());
-		}
-	
-		int readFd(int sockfd);
-		uint32_t readAll(std::string& data);
-		uint32_t readUntilCrlf(std::string& data);
-	
-		//bool append();
-	
-		uint32_t bufferSize() const 
-		{ 
-			return _buffer.size(); 
-		}
-	
-	private:
-		char* begin()
-		{ return &*_buffer.begin(); }
+        uint32_t writableBytes() const
+        {  return _buffer.size() - _writerIndex; }
 
-		const char* begin() const
-		{ return &*_buffer.begin(); }
-	
-		char* beginWrite()
-		{ return begin() + _writerIndex; }
+        char* peek() 
+        { return begin() + _readerIndex; }
 
-		const char* beginWrite() const
-		{ return begin() + _writerIndex; }
+        const char* peek() const
+        { return begin() + _readerIndex; }
 
-		std::vector<char> _buffer;
-		size_t _readerIndex = 0;
-		size_t _writerIndex = 0;
-	
-		static const char kCRLF[];
-		static const uint32_t MAX_BYTES_PER_READ = 2048;
-		static const uint32_t MAX_BUFFER_SIZE = 102400;
-	};
+        const char* findFirstCrlf() const
+        {    
+            const char* crlf = std::search(peek(), beginWrite(), kCRLF, kCRLF+2);
+            return crlf == beginWrite() ? nullptr : crlf;
+        }
+
+        const char* findLastCrlf() const
+        {    
+            const char* crlf = std::find_end(peek(), beginWrite(), kCRLF, kCRLF+2);
+            return crlf == beginWrite() ? nullptr : crlf;
+        }
+
+        void retrieveAll() 
+        { 
+            _writerIndex=0; 
+            _readerIndex=0; 
+        }
+
+        void retrieve(size_t len)
+        {
+            if (len <= readableBytes())
+            {
+                _readerIndex += len;
+                if(_readerIndex == _writerIndex)
+                {
+                    _readerIndex = 0;
+                    _writerIndex = 0;
+                }
+            }
+            else
+            {
+                retrieveAll();
+            }
+        }
+
+        void retrieveUntil(const char* end)
+        { retrieve(end - peek()); }
+
+        int readFd(int sockfd);
+        uint32_t readAll(std::string& data);
+        uint32_t readUntilCrlf(std::string& data);
+
+        //bool append();
+
+        uint32_t bufferSize() const 
+        { return _buffer.size(); }
+
+    private:
+        char* begin()
+        { return &*_buffer.begin(); }
+
+        const char* begin() const
+        { return &*_buffer.begin(); }
+
+        char* beginWrite()
+        { return begin() + _writerIndex; }
+
+        const char* beginWrite() const
+        { return begin() + _writerIndex; }
+
+        std::vector<char> _buffer;
+        size_t _readerIndex = 0;
+        size_t _writerIndex = 0;
+
+        static const char kCRLF[];
+        static const uint32_t MAX_BYTES_PER_READ = 2048;
+        static const uint32_t MAX_BUFFER_SIZE = 102400;
+    };
 }
 
 #endif
