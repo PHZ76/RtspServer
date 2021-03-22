c08da08e79c5cb735f969f4341201ca41bae5a61
scottxu <scottxu@scottxublog.com>
2021-03-23 00:14:46 +0800

    Update AACSource.cpp
    Fix errors in AACSampleRate.  (ISO-IEC13818-7 Table 35)

--- src/xop/AACSource.cpp
+++ src/xop/AACSource.cpp
@@ -47,10 +47,10 @@ string AACSource::GetMediaDescription(uint16_t port)
 
 static uint32_t AACSampleRate[16] =
 {
-	97000, 88200, 64000, 48000,
-	44100, 32000, 24000, 22050,
-	16000, 12000, 11025, 8000,
-	7350, 0, 0, 0 /*reserved */
+	96000, 88200, 64000, 48000,
+        44100, 32000, 24000, 22050,
+        16000, 12000, 11025, 8000,
+        7350, 0, 0, 0 /*reserved */
 };
 
 string AACSource::GetAttribute()  // RFC 3640
