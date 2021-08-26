3f02c303658bfc278c2ff451ce23a15030699a5f
phz76 <penghaoze76@qq.com>
2021-08-25 19:35:35 -0700

    update

--- src/xop/AACSource.cpp
+++ src/xop/AACSource.cpp
@@ -46,7 +46,7 @@ string AACSource::GetMediaDescription(uint16_t port)
 
 static uint32_t AACSampleRate[16] =
 {
-	97000, 88200, 64000, 48000,
+	96000, 88200, 64000, 48000,
 	44100, 32000, 24000, 22050,
 	16000, 12000, 11025, 8000,
 	7350, 0, 0, 0 /*reserved */
